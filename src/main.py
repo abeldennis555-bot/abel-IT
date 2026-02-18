c9bf84203a1399427176e6c15a4e6141e0be3ec3
Abel <yourgithubemail@gmail.com>
2026-02-19 00:29:15 +0530

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+print("Python Version:", sys.version)
+print("Current Working Directory:", os.getcwd())
+
+name = "Aslin"
+print(f"Hello, {name}! Welcome to the professional Python setup.")
